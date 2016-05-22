d59e5ca3e84ab0b88e6d777e8417be9bfa0d17b3
suzan2go <ksuzuki180@gmail.com>
2016-05-22 23:16:55 +0900

    create default fome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root to: 'home#index'
+
   get  '/auth/:provider/callback', to:'sessions#callback'
   post '/auth/:provider/callback', to: 'sessions#callback'
   get  '/auth/failure' => 'sessions#failure'
