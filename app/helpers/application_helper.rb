2c05d11a51c93e936e473bd8a978e996c5e98d79
suzan2go <ksuzuki180@gmail.com>
2016-05-23 00:14:34 +0900

    move from sprockets to webpack assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
-# encoding: utf-8
 # frozen_string_literal: true
 module ApplicationHelper
+  def webpack_asset_path(path)
+    if Rails.env.development? || Rails.env.test?
+      return "http://localhost:3500/assets/#{path}"
+    end
+    host = Rails.application.config.action_controller.asset_host
+    manifest = Rails.application.config.assets.webpack_manifest
+    path = manifest[path] if manifest && manifest[path].present?
+    "#{host}/assets/#{path}"
+  end
 end
