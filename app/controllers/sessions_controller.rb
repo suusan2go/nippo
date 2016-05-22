1a982a0f0f54072a56fc8287eaceb43d2f29bbf6
suzan2go <ksuzuki180@gmail.com>
2016-05-22 21:14:54 +0900

    rubocop enable

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# frozen_string_literal: true
+class SessionsController < ApplicationController
+  def create
+  end
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
+# encoding: utf-8
+# frozen_string_literal: true
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google[:client_id], Rails.application.secrets.google[:client_secret], { name: 'google' }
+  provider :google_oauth2,
+           Rails.application.secrets.google['client_id'],
+           Rails.application.secrets.google['client_secret'], name: 'google'
 end
-
