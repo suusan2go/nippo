e8a51cf1ad6e83ebe6478345a8e87c954959593b
suzan2go <ksuzuki180@gmail.com>
2017-01-15 19:37:11 +0900

    import grom echigoya

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
-# frozen_string_literal: true
 module ApplicationHelper
   def webpack_asset_path(path)
-    if Rails.env.development? || Rails.env.test?
-      return "http://localhost:3500/assets/#{path}"
-    end
-    host = Rails.application.config.action_controller.asset_host
+    return "http://localhost:3500/assets/#{path}" if Rails.env.development?
     manifest = Rails.application.config.assets.webpack_manifest
     path = manifest[path] if manifest && manifest[path].present?
-    "#{host}/assets/#{path}"
+    "/assets/#{path}"
   end
 end
