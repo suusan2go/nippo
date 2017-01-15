351c82a9e640b2353d0f516e491fd2c595aa542e
suzan2go <ksuzuki180@gmail.com>
2017-01-15 22:58:34 +0900

    import initializer

---
+++ config/initializers/hypernova.rb
@@ -0,0 +1,4 @@
+Hypernova.configure do |config|
+  config.host = 'hypernova'
+  config.port = 3030 # The port where the node service is listening
+end
--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-# frozen_string_literal: true
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           Rails.application.secrets.google['client_id'],
-           Rails.application.secrets.google['client_secret'], name: 'google'
-end
