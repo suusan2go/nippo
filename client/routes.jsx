71cbf5ab3eca8aa2416fdf13af2cbf883acfc72d
suzan2go <ksuzuki180@gmail.com>
2017-01-23 01:21:14 +0900

    remove logout component

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import Top from 'components/Top';
 import ArticleList from 'containers/ArticleList';
-import DefaultLayout from 'containers/Layout/DefaultLayout';
 import LoginLayout from 'containers/Layout/LoginLayout';
 
 export default (
   <Route path="/">
-    <Route component={DefaultLayout} >
-      <IndexRoute component={Top} />
-    </Route>
     <Route component={LoginLayout} >
-      <Route path="articles" component={ArticleList} />
+      <IndexRoute component={ArticleList} />
     </Route>
   </Route>
 );
