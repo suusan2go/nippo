295e9e93f68648d582452edfa1405bfda244cb7e
suzan2go <ksuzuki180@gmail.com>
2017-01-24 21:47:26 +0900

    fix annnotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@
 #  id         :integer          not null, primary key
 #  name       :string           not null
 #  email      :string           not null
-#  avatr_url  :string           not null
+#  avatar_url :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
