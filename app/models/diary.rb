c4490bddf051f6ab3ea197c54a05437316131539
suzan2go <ksuzuki180@gmail.com>
2017-01-24 22:03:03 +0900

    add dirary model

---
+++ app/models/diary.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: diaries
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  body       :text             not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_diaries_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_f03fd03c63  (user_id => users.id)
+#
+
+class Diary < ApplicationRecord
+  belongs_to :user
+end
