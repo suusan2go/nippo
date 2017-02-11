c7689912c5f42b14a02a50411cf29839352fd488
suzan2go <ksuzuki180@gmail.com>
2017-02-12 00:17:38 +0900

    add flowtype for api

--- client/javascripts/api/index.js
+++ client/javascripts/api/index.js
@@ -1,32 +1,33 @@
+// @flow
 import axios from 'axios';
 import authenticityToken from './authenticityToken';
 
 axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(
-  (config) => {
+  (config: Object) => {
     config.headers['X-CSRF-TOKEN'] = authenticityToken();  // eslint-disable-line
     return config;
   },
   error => Promise.reject(error),
 );
 
-function sendDelete(url) {
+function sendDelete(url: string) {
   return axios.delete(url).then(response => response.data);
 }
 
-function sendPatch(url, data) {
+function sendPatch(url: string, data: Object) {
   return axios.patch(url, data).then(response => response.data);
 }
 
-function sendPost(url, data) {
+function sendPost(url: string, data: Object) {
   return axios.post(url, data).then(response => response.data);
 }
 
-export function createDiaryEntry({ title, body }) {
+export function createDiaryEntry({ title, body }: { title: string, body: string}) {
   return sendPost('/api/diary_entries', { diary_entry: { title, body } });
 }
 
-export function updateDiaryEntry({ diaryId, title, body }) {
+export function updateDiaryEntry({ diaryId, title, body }: { diaryId: number, title: string, body: string }) {
   return sendPatch(`/api/diary_entries/${diaryId}`, { title, body });
 }
