82450305cab5620608f943193010150c63b5775f
suzan2go <ksuzuki180@gmail.com>
2017-02-26 22:57:23 +0900

    redirect to subdomin after sign_in or sign_up

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -3,8 +3,13 @@ class AuthsController < ApplicationController
   skip_before_action :require_sign_in
 
   def callback
-    sign_in(user: User.find_or_create_from_omniauth(omniauth))
-    redirect_to root_path
+    user = User.find_or_create_from_omniauth(omniauth)
+    sign_in(user: user)
+    if user.family
+      redirect_to root_url(subdomain: user.family.slug)
+    else
+      redirect_to invitation_url(subdomain: Family.register(registerer: user).slug)
+    end
   end
   alias google callback
 
